cad1ab45f298857faa3156f6c8cbf1914b6e1a70
antoniofsiqueira <antonio.fsiqueira@gmail.com>
2020-02-04 11:42:45 -0300

    Finalizado!

--- lib/tiles/order_tile.dart
+++ lib/tiles/order_tile.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class OrderTile extends StatelessWidget {
+  
   final String orderId;
 
   OrderTile(this.orderId);
@@ -15,16 +16,14 @@ class OrderTile extends StatelessWidget {
         child: Padding(
           padding: EdgeInsets.all(8.0),
           child: StreamBuilder<DocumentSnapshot>(
-              stream: Firestore.instance
-                  .collection("orders")
-                  .document(orderId)
-                  .snapshots(),
-              builder: (context, snapshot) {
-                if (!snapshot.hasData)
+              stream: Firestore.instance.collection("orders").document(orderId).snapshots(),
+              builder: (context, snapshot){
+                if(!snapshot.hasData)
                   return Center(
                     child: CircularProgressIndicator(),
                   );
                 else {
+
                   int status = snapshot.data["status"];
 
                   return Column(
@@ -34,20 +33,16 @@ class OrderTile extends StatelessWidget {
                         "Código do pedido: ${snapshot.data.documentID}",
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
-                      SizedBox(
-                        height: 4.0,
+                      SizedBox(height: 4.0,),
+                      Text(
+                        _buildProductsText(snapshot.data)
                       ),
-                      Text(_buildProductsText(snapshot.data)),
-                      SizedBox(
-                        height: 4.0,
-                      ),
+                      SizedBox(height: 4.0,),
                       Text(
                         "Status do Pedido:",
                         style: TextStyle(fontWeight: FontWeight.bold),
                       ),
-                      SizedBox(
-                        height: 4.0,
-                      ),
+                      SizedBox(height: 4.0,),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: <Widget>[
@@ -69,40 +64,35 @@ class OrderTile extends StatelessWidget {
                     ],
                   );
                 }
-              }),
-        ));
+              }
+          ),
+        )
+    );
   }
 
-  String _buildProductsText(DocumentSnapshot snapshot) {
+  String _buildProductsText(DocumentSnapshot snapshot){
     String text = "Descrição:\n";
-    for (LinkedHashMap p in snapshot.data["produtos"]) {
-      text +=
-          "${p["quantity"]} x ${p["product"]["title"]} (R\$ ${p["product"]["price"].toStringAsFixed(2)})\n";
+    for(LinkedHashMap p in snapshot.data["products"]){
+      text += "${p["quantity"]} x ${p["product"]["title"]} (R\$ ${p["product"]["price"].toStringAsFixed(2)})\n";
     }
     text += "Total: R\$ ${snapshot.data["totalPrice"].toStringAsFixed(2)}";
     return text;
   }
 
-  Widget _buildCircle(
-      String title, String subtitle, int status, int thisStatus) {
+  Widget _buildCircle(String title, String subtitle, int status, int thisStatus){
+
     Color backColor;
     Widget child;
 
-    if (status < thisStatus) {
+    if(status < thisStatus){
       backColor = Colors.grey[500];
-      child = Text(
-        title,
-        style: TextStyle(color: Colors.white),
-      );
-    } else if (status == thisStatus) {
+      child = Text(title, style: TextStyle(color: Colors.white),);
+    } else if (status == thisStatus){
       backColor = Colors.blue;
       child = Stack(
         alignment: Alignment.center,
         children: <Widget>[
-          Text(
-            title,
-            style: TextStyle(color: Colors.white),
-          ),
+          Text(title, style: TextStyle(color: Colors.white),),
           CircularProgressIndicator(
             valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
           )
@@ -110,10 +100,7 @@ class OrderTile extends StatelessWidget {
       );
     } else {
       backColor = Colors.green;
-      child = Icon(
-        Icons.check,
-        color: Colors.white,
-      );
+      child = Icon(Icons.check, color: Colors.white,);
     }
 
     return Column(
@@ -126,5 +113,6 @@ class OrderTile extends StatelessWidget {
         Text(subtitle)
       ],
     );
+
   }
 }
